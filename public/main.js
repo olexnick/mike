195de6bb7bd0dd7db0be422ecd53608258a2805c
olexnick <olexnick@gmail.com>
2017-08-21 21:18:42 +0300

    some changes

---
+++ public/main.js
@@ -0,0 +1,72 @@
+$(window).scroll(function(){
+
+  var st = $(this).scrollTop();
+  console.log(st, "сдвиг по высоте");
+  $(".text").css({
+    "transform" : "translate3d(0px,  " + st/15  + "%, .01px)",
+		"-webkit-transform" : "translate3d(0px,  " + st/15  + "%, .01px)"
+  });
+
+
+  var st2 = $(this).scrollTop();
+  $(".head-parallax img").css({
+    "transform" : "translate3d(0px,  - " + st2/10 + "%, .01px)",
+		"-webkit-transform" : "translate3d(0px,  -"  + st2/10  + "%, .01px)",
+    "top" :  - st2/80  + "%",
+  });
+
+  var st3 = $(this).scrollTop();
+  $(".head-parallax-carosel").css({
+    "transform" : "translate3d(0px,  - " + st3/10 + "%, .01px)",
+		"-webkit-transform" : "translate3d(0px,  -"  + st3/10  + "%, .01px)",
+    "top" :  - st3/80  + "%",
+  });
+  console.log(st3/80, "состояние высоты картинки");
+
+
+  // var jumboHeight = $('.head-parallax-wrapper').outerHeight();
+  // var st3 = $(this).scrollTop();
+  // if (st3 <= 500){
+  //     $(".head-parallax-wrapper").css({
+  //       "height" :  + jumboHeight-(st3/400),
+  //       "color" :"red"
+  //     });
+  // } else {
+  //   $(".head-parallax-wrapper").css({
+  //     "height" : + jumboHeight+(st3/100),
+  //     "color" :"green"
+  //   });
+  // }
+  //
+  // console.log(st3, "наша st3");
+  // console.log(st3/100, "наша st3");
+  // $(".head-parallax-wrapper").css({
+  //   // "transform" : "translate3d(0px,  - " + st3/6  + "%, .01px)",
+	// 	// "-webkit-transform" : "translate3d(0px,  -"  + st3/6  + "%, .01px)",
+  //
+  //     "height" :  + jumboHeight-(st3/100),
+  //     "color" :"red"
+  //   });
+
+
+
+
+  // console.log(jumboHeight-(st3/100), "состояние высоты wrapper");
+
+
+
+
+});
+$('.parallax-window').parallax({imageSrc: "./img/center.jpg"});
+
+// var jumboHeight = $('.').outerHeight();
+// console.log(jumboHeight, "до высота");
+// function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     $('.head-parallax-wrapper').css('height', (jumboHeight-scrolled) + 'px');
+//     console.log(jumboHeight-scrolled, "после Высота");
+// }
+//
+// $(window).scroll(function(e){
+//     parallax();
+// });
